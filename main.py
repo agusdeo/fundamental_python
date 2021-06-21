6a207f876cf90fdf1c55926b343992c6fa4d7be9
Agus Deo Saputra Utama <agusdeosaputra@gmail.com>
2021-06-21 21:27:00 +0700

    selesai belajar fundamental pythons

---
+++ main.py
@@ -0,0 +1,25 @@
+# ini adalah untuk ngeprint kata kata
+print("Hello Agus")
+print('Apa kabar pagi ini')
+print('Mari segera menghasilkan dari koding bersamaku')
+
+# ini untuk belajar percabangan
+ingin_cepat = False
+if ingin_cepat:
+    print('Belajar Yang Rajin')
+else:
+    print('Malas Belajar')
+
+# ini untuk perulangan
+jumlah_anak = 3
+
+for index_anak in range(1, 6):
+    print(f'Halo Anak ke {index_anak}')
+
+# ini untuk array
+anak = ['Eko', 'Agus', 'Deo', 'Saputra']
+print(anak)
+print (f'Hai {anak[1]}')
+
+for a in anak :
+    print(f'\nHai {a}')
